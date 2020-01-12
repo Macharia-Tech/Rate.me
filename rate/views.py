7671ad7619a08ed5484f523a39cd0a59e0dc8bcf
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-13 01:04:22 +0300

    added a login form for the user to be authenticated

--- rate/views.py
+++ rate/views.py
@@ -4,6 +4,3 @@ from django.http import HttpResponse,Http404
 # Create your views here.
 def start (request):
     return HttpResponse (' start.html')
-
-    except ValueError:
-        #Raise 404 error when ValueError is thrown raise Http404()            
