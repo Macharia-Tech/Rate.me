9122cdbfc21f2328e2a1ff19aa0103b9c6a82499
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-12 22:34:22 +0300

    created the urls.py to connect the functions called in the views.py

--- rate/views.py
+++ rate/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
 
 # Create your views here.
+def start (request):
+    return HttpResponse (' Project Rator')
+
+    except ValueError:
+        #Raise 404 error when ValueError is thrown raise Http404() 
